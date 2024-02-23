2a8f152e041d5f774f071214c35df8417a84c399
Pratyaksh <pratyaksh0610@gmail.com>
2024-02-23 18:09:54 +0530

    Context Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,104 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Suspense, lazy, useContext, useState } from "react";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
-import { Dashboard } from "./components/Dashboard";
-import { Landing } from "./components/Landing";
+import { CountContext } from "./context";
+const Dashboard = lazy(() => import("./components/Dashboard"));
+const Landing = lazy(() => import("./components/Landing"));
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/" element={<Landing />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <CountContext.Provider
+        value={{
+          count,
+          setCount,
+        }}
+      >
+        <Count />
+      </CountContext.Provider>
+    </>
+    // <>
+    //   <BrowserRouter>
+    //     <Appbar />
+    //     <Routes>
+    //       <Route
+    //         path="/dashboard"
+    //         element={
+    //           <Suspense fallback={"loading..."}>
+    //             {" "}
+    //             <Dashboard />
+    //           </Suspense>
+    //         }
+    //       />
+    //       <Route
+    //         path="/"
+    //         element={
+    //           <Suspense fallback={"loading.."}>
+    //             <Landing />
+    //           </Suspense>
+    //         }
+    //       />
+    //     </Routes>
+    //   </BrowserRouter>
+    // </>
+  );
+}
+
+function Count() {
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons />
+    </div>
+  );
+}
+function CountRenderer() {
+  const { count, setCount } = useContext(CountContext);
+  return <div>{count}</div>;
+}
+function Buttons() {
+  const { count, setCount } = useContext(CountContext);
+  // const setCount = useContext(CountContext);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrease
+      </button>
+    </div>
+  );
+}
+
+function Appbar() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Landing page
+      </button>
+
+      <button
+        onClick={() => {
+          navigate("/dashboard");
+        }}
+      >
+        Dashboard page
+      </button>
+    </div>
   );
 }
 
