8be9a314d619836bac6f961453f2447853603ee5
Pratyaksh <pratyaksh0610@gmail.com>
2024-02-24 21:38:59 +0530

    Asynchronous Data queries in atom

--- week 7 easy bits/src/atoms.jsx
+++ week 7 easy bits/src/atoms.jsx
@@ -1,14 +1,18 @@
+import axios from "axios";
 import { atom, selector } from "recoil";
 
 //Asynchronous Data queries
 export const notifications = atom({
   key: "networkAtom",
-  default: {
-    network: 0,
-    jobs: 0,
-    messaging: 0,
-    notifications: 0,
-  },
+  default: selector({
+    key: "networkAtomSelector",
+    get: async () => {
+      const res = await axios.get(
+        "https://sum-server.100xdevs.com/notifications"
+      );
+      return res.data;
+    },
+  }),
 });
 
 export const totalNotificationSelector = selector({
